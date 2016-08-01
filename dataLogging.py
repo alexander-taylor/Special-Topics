d0543dc941ab978675c38561745091d303ba886c
Alex Taylor <alexander.taylor1@uqconnect.edu.au>
2016-08-02 09:30:50 +1000

    started datalogging file. creates user selected directory and can create scan subfolders

--- dataLogging.py
+++ dataLogging.py
@@ -5,3 +5,84 @@ Created on Fri Jul 29 10:30:34 2016
 @author: alexa_000
 """
 
+import tkinter as tk
+import tkinter.filedialog
+import sys
+import os
+import time
+
+
+
+#==============================================================================
+# File for logging experimental data that with primarily consist of spatial scans.
+#     -would like to save files in global folder with sub folders, 1 for each line or column of the
+#     scan.
+#     -would either like to have naming convention which contains appropriate info of the scan or
+#     perhaps a standard header for each file.close
+#     -maybe a named tuple could be saved for header info and an element could be the actual data.
+#==============================================================================
+
+#==============================================================================
+# Probably going to avoid using sqlite3 or pickles or any of that nonsense.
+# Need to let user select path for saved data scan.
+# Need a time stamp, for scan creation
+#==============================================================================
+
+#==============================================================================
+# Will need to create a scan data class.
+# Should store current scan path, current scan folder
+#==============================================================================
+
+#==============================================================================
+# Want to create a text file in the main scan folder with information about the scan
+# need timestamp, scan size, pixels etc, scope settings
+#==============================================================================
+
+class scan:
+    def __init__(self):
+        self.scanPath = ''
+        self.subPaths = []
+
+    def select_scan_path(self):
+        
+        root = tk.Tk()    
+        
+        path = tk.filedialog.askdirectory()
+        
+        if(path == ''):
+            print("User cancelled path select\n")
+        else:
+            print("Data Path: " + path +"\n")
+            
+        root.quit()        
+            
+        self.scanPath = path
+    
+    def create_scan_folder(self):
+        
+        self.scanPath = select_scan_path()
+        
+        return
+        
+    def create_subfolders(self,num):
+        
+        for i in range(1,num+1):
+
+            subpath = self.scanPath + "/" + str(i)           
+            print(subpath)
+            self.subPaths.append(subpath)
+            if not os.path.exists(subpath):           
+                os.mkdir(self.scanPath + "/" + str(i))
+                
+        return
+        
+    def save_data(path):
+        
+        
+        return
+    
+    
+    
+    
+    
+    
--- zabers.py
+++ zabers.py
@@ -5,3 +5,21 @@ Created on Fri Jul 29 10:30:10 2016
 @author: alexa_000
 """
 
+from zaber.serial import *
+
+#==============================================================================
+# Required functionality:
+#     -home command
+#     -renumber command
+#     -abs move
+#     -rel move
+#     -set origin
+#     -set limits
+#     -arbitrary scan, i.e. x by y pixels, with defined step size
+#     -perhaps also arbitrary scan with x by y size in mm and generate approx step size.
+#     -need to be able to save step size in both encoder values and um for each zaber device created
+#     -need to be able to specify 2-axis or 3-axis
+#     -should probably do this with classes, need to revise some pyhton
+#==============================================================================
+
+
